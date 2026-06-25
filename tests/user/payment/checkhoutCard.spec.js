e40a58eb2828fcbf0fa0c7e87e7145a05a9ac1e9
Vindi Sovia A <vindisovia@gmail.com>
2026-06-25 13:35:52 +0700

    Validasi Chechout

--- tests/user/payment/checkhoutCard.spec.js
+++ tests/user/payment/checkhoutCard.spec.js
@@ -2,6 +2,7 @@
 
 const { test, expect } = require('@playwright/test');
 const { loginUser } = require('../../../helpers/loginUser');
+const { TIMEOUT } = require('node:dns');
 
 test.describe('Checkout From Catalog All Payment', () => {
 
@@ -15,10 +16,24 @@ test.describe('Checkout From Catalog All Payment', () => {
     const iconKeranjang = '[data-icon="akar-icons:cart"]';
     await page.locator(iconKeranjang).click();
 
-    const checkAll = '[data-testid="keranjang-belanja-input-order_---i---"]';
-    await page.locator(checkAll).click();
+    const checkboxes = page.locator('[id^="chk-pilih-principle-"]');
+    
+    // Tunggu sampai minimal 1 checkbox muncul
+    await checkboxes.first().waitFor({
+      state: 'visible',
+      timeout: 15000, // 10 detik
+});
 
-    const buttonCheckout = '[data-testid="keranjang-belanja-button-btn-keranjang-belanja-5"]';
+    const total = await checkboxes.count();
+
+    for (let i = 0; i < total; i++) {
+    if (!(await checkboxes.nth(i).isChecked())) {
+        await checkboxes.nth(i).check();
+    }
+}
+
+
+    const buttonCheckout = '[data-testid="keranjang-belanja-btn-checkout"]';
     await page.locator(buttonCheckout).click();
 
     const buttonPayment = '#btn-checkout-1';
--- tests/user/payment/checkoutEwallet.spec.js
+++ tests/user/payment/checkoutEwallet.spec.js
@@ -8,9 +8,10 @@ test.describe('Checkout From Catalog All Payment', () => {
   test.beforeEach(async ({ page }) => {
     await loginUser(page);
   })
-  
-  test('checkout From Catalog with saldo', async ({ page }) => {
-    const buttonKatalog = '#mobile-top-nav > div > div:nth-child(2)';
+
+  test('Checkout payment with ewallet', async ({ page }) => {
+
+        const buttonKatalog = '#mobile-top-nav > div > div:nth-child(2)';
     await page.locator(buttonKatalog).click();
 
     const iconKeranjang = '[data-icon="akar-icons:cart"]';
@@ -36,23 +37,9 @@ test.describe('Checkout From Catalog All Payment', () => {
     const buttonCheckout = '[data-testid="keranjang-belanja-btn-checkout"]';
     await page.locator(buttonCheckout).click();
 
-    const buttonPayment = '#btn-checkout-1';
-    await page.locator(buttonPayment).click();
 
-    //pilih metode
-    const checkboxEwallet = page.locator('[data-testid="pilih-pembayaran-input-e-wallet"]').first();
-    await checkboxEwallet.waitFor({ state: 'visible' });
-    await checkboxEwallet.click({ force: true });
-
-    const buttonBayar = '[data-testid="pilih-pembayaran-button-btn-pilih-pembayaran-1"]';
-    await page.locator(buttonBayar).click();
-
-    const buttonKponfirmasi = '[class="swal2-confirm swal2-styled"]';
-    await page.locator(buttonKponfirmasi).click();
-    await expect(page.getByText('Pembayaran Berhasil !')).toBeVisible();
-
-    const buttonPesanan = '[data-testid="pembayaran-berhasil-button-btn-pembayaran-berhasil-1"] ';
-    await page.locator(buttonPesanan).click();
+    
   })
+
 
 });
